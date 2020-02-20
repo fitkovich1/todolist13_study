5dadf7fd67f585c341419a1473c28011c196c761
fitkovich1 <fitkovich1@gmai.com>
2020-02-20 22:56:52 +0300

    Updates

---
+++ precache-manifest.f2cca2c45046783c48fdf06215516ff2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d99db6a5189e76e1b08f98dc0daaa2bb",
+    "url": "/todolist13_study/index.html"
+  },
+  {
+    "revision": "c58e3495c0da32ded9e3",
+    "url": "/todolist13_study/static/css/main.a4a1cece.chunk.css"
+  },
+  {
+    "revision": "3a4d2ffb0330d081124b",
+    "url": "/todolist13_study/static/js/2.eeaa9662.chunk.js"
+  },
+  {
+    "revision": "c58e3495c0da32ded9e3",
+    "url": "/todolist13_study/static/js/main.957f48c7.chunk.js"
+  },
+  {
+    "revision": "8ce7d1e25da0d1142d7c",
+    "url": "/todolist13_study/static/js/runtime~main.34aabb43.js"
+  }
+]);
